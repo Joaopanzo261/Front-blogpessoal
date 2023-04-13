d3d54009b8872a43bcb6d6c674ac5bd3a2ddfd57
Joaopanzo261 <joaopanzo261@gmail.com>
2023-04-12 23:25:26 -0300

    Navbar e Home de BlogPessoal

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <>
+            <h1>Footer</h1>
+        
+        
+        
+        
+        </>
+
+
+
+    )
+
+}
+
+export default Footer;
