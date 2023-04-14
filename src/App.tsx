d0d989addae85b4d2c767a871e73afa94457e23d
Joaopanzo261 <joaopanzo261@gmail.com>
2023-04-13 23:01:20 -0300

    Blog atualizado com Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './components/home/Home';
+import Home from './assets/paginas/home/Home';
+import Login from './assets/paginas/login/Login';
 
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-   
-    </>
+    <Router>
+        <Navbar />
+          <Switch>
+              <div style={{minHeight:'100vh'}}>
+
+              <Route exact path='/'>
+                    < Login />
+                </Route>
+
+              <Route path='/login'>
+                    < Login />
+                </Route>
+
+                <Route path='/home'>
+                    < Home />
+                </Route>
+              </div>
+          </Switch>
+
+        <Footer />
+    </Router>
   );
 }
 
