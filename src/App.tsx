e7014dd70bc7f0fadfc13867949354fadc1ff2f6
Joaopanzo261 <joaopanzo261@gmail.com>
2023-04-21 21:17:49 -0300

    listaTema e listaPostagem de Blogpessoal

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import CadastroUsuario from './assets/paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/ListaTema';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './assets/paginas/home/Home';
 import Login from './assets/paginas/login/Login';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
               <Route path="/login" element={<Login />} />
              <Route path="/home" element={<Home />} />
              <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+             <Route path="/temas" element={<ListaTema />} />
+             <Route path="/posts" element={<ListaPostagem />} />
              
         </Routes>
       </div>
